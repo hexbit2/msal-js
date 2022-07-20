769dde1fc5c0c8f52f8d79eeb68bb4827cef6484
hexbit2 <nawazedu1774@gmail.com>
2022-07-20 15:07:32 -0400

    first commit

---
+++ app/authConfig.js
@@ -0,0 +1,56 @@
+const msalConfig = {
+    auth: {
+        // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
+        clientId: "bb4ae21d-5178-4c27-b549-2c3149d63f56",
+        // Full directory URL, in the form of https://login.microsoftonline.com/<tenant>
+        authority: "https://login.microsoftonline.com/7b92c877-5b08-4dde-b025-ae827f46bfed",
+        // Full redirect URL, in form of http://localhost:3000
+        redirectUri: "http://localhost:3002",
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {	
+        loggerOptions: {	
+            loggerCallback: (level, message, containsPii) => {	
+                if (containsPii) {		
+                    return;		
+                }		
+                switch (level) {		
+                    case msal.LogLevel.Error:		
+                        console.error(message);		
+                        return;		
+                    case msal.LogLevel.Info:		
+                        console.info(message);		
+                        return;		
+                    case msal.LogLevel.Verbose:		
+                        console.debug(message);		
+                        return;		
+                    case msal.LogLevel.Warning:		
+                        console.warn(message);		
+                        return;		
+                }	
+            }	
+        }	
+    }
+};
+
+/**
+ * Scopes you add here will be prompted for user consent during sign-in.
+ * By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+ * For more information about OIDC scopes, visit: 
+ * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+ */
+ const loginRequest = {
+    scopes: ["User.Read"]
+};
+
+/**
+ * Add here the scopes to request when obtaining an access token for MS Graph API. For more information, see:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+const tokenRequest = {
+    scopes: ["User.Read", "Mail.Read"],
+    forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
+};
